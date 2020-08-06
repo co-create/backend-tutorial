85048f886022efdfb174dc69e7e87af7fde79972
Ubuntu <ubuntu@ip-172-31-9-33.us-west-1.compute.internal>
2020-08-06 00:46:47 +0000

    refactor from thanktank, set up web server

--- user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    jwt = models.CharField(max_length=200)
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.loginAttempt, name='login'),
-    path('all', views.get_users, name='all'),
-]
